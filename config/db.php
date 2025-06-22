442707828afa3aa14ea1fe108bfa33de2243c1f1
Praveen Yadav <praveenyadav@nickelfox.com>
2025-06-22 15:33:18 +0530

    db configured

--- config/db.php
+++ config/db.php
@@ -4,7 +4,7 @@ return [
     'class' => 'yii\db\Connection',
     'dsn' => 'mysql:host=localhost;dbname=yii2basic',
     'username' => 'root',
-    'password' => '',
+    'password' => 'Pr@veen_12345!',
     'charset' => 'utf8',
 
     // Schema cache options (for production environment)
