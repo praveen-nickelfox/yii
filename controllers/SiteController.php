ff2348921e662f3135c4ec4203cf93ffacff31aa
Praveen Yadav <praveenyadav@nickelfox.com>
2025-06-22 15:34:27 +0530

    say hello

--- controllers/SiteController.php
+++ controllers/SiteController.php
@@ -9,6 +9,7 @@ use yii\web\Response;
 use yii\filters\VerbFilter;
 use app\models\LoginForm;
 use app\models\ContactForm;
+use app\models\EntryForm;
 
 class SiteController extends Controller
 {
@@ -124,5 +125,12 @@ class SiteController extends Controller
     public function actionAbout()
     {
         return $this->render('about');
+    }
+
+    public function actionSay($message = 'Hello!')
+    {
+        return $this->render('say', [
+            'message' => $message,
+        ]);
     }
 }
